783efda7dcf4643b9284aa3b89502d0c6c60d6a0
Harsh Maheshweri <harshmaheshweri0@gmail.com>
2025-04-17 18:21:16 +0000

    datatypes

---
+++ 01_basic/DataType.js
@@ -0,0 +1,24 @@
+"user strict"; //treate all JS xide as newwer version
+// alert(3 + 3); // we are using nodejs., not browser
+console.log(3
+    +
+    3) // code readibility should be high
+ console.log("Hitesh");
+
+ let name = "Harsh";
+ let age = 21;
+ let isLog = true;
+ let state;
+
+ // number => 2 to power 53
+ // bigint 
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined =>
+ // symbol => unique
+
+ // object
+
+ console.log(typeof undefined); // undefined
+ console.log(typeof null); // object
---
+++ 01_basic/var.js
@@ -0,0 +1,7 @@
+const userId = 1; 
+let userNum = 123;
+var userCity = "Jaipur";
+userState = "Rajasthan";
+userNum = 111;
+userCity = "Sheopur";
+console.table([userId, userNum, userCity, userState]);
